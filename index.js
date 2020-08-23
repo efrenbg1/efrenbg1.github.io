6d394c6d5375821724a233777b2d6a76a4d4658d
Efrén Boyarizo <efren@boyarizo.es>
2020-08-23 20:58:10 +0200

    Added links

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 
-let req = new XMLHttpRequest();
-req.open("GET", 'db.json', true);
-req.responseType = 'json';
-req.onload = function () {
-    if (navigator.languages.includes('es')) {
-        var lang = req.response['es'];
-    } else {
-        var lang = req.response['en'];
+var translations = {
+    "en": {
+        "about-me": "About me:",
+        "about": "Born and raised in Madrid (Spain) I've been programming from an early age, always looking for the simplification of daily tasks. This way I came up with the idea of lemonSW.",
+        "subtitle": "Student and part-time freelancer.",
+        "highschool": "Graduated of Bachillerato de Excelencia of science from I.E.S. el Burgo de las Rozas.",
+        "upm": "Currently studying Grado en Ingeniería y Servicios de las Telecomunicaciones in the UPM."
+    },
+    "es": {
+        "about-me": "Sobre mi:",
+        "about": "Nacido en Madrid, me he dedicado a la programación desde pequeño. Me apasiona aprender cosas nuevas e involucrarme en proyectos que implican superación tanto personal como grupal.",
+        "subtitle": "Estudiante y freelancer a tiempo parcial.",
+        "highschool": `Graduado en <i>Bachillerato de Excelencia</i> en ciencias por el <a href="https://www.educa2.madrid.org/web/iesburgoiecheverria.lasrozas">IES El Burgo</a>.`,
+        "upm": `Actualmente estudiando <i>Grado en Ingeniería y Servicios de las Telecomunicaciones</i> en la <a href="https://www.upm.es/">UPM</a>.`
     }
-    Object.keys(lang).forEach((n) => {
-        document.getElementById(n).innerText = lang[n];
-    });
 };
-req.send();
+
+if (navigator.languages.includes('es')) {
+    var lang = translations['es'];
+} else {
+    var lang = translations['en'];
+}
+Object.keys(lang).forEach((n) => {
+    document.getElementById(n).innerHTML = lang[n];
+});
+feather.replace();
